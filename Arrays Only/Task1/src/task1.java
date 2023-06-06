d7983ac197a11dd92524a8f96f4392882eaec855
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-06 21:53:48 +0530

    Update task1.java

--- Arrays Only/Task1/src/task1.java
+++ Arrays Only/Task1/src/task1.java
@@ -20,4 +20,64 @@ public class task1 {
         int Pump1free = 0;
         int Pump2free = 0;
         int Pump3free = 0;
+
+        int menuCount = 1;
+
+        System.out.println("*****Fuel Queue Management System.*****\n");
+
+        System.out.println("100 or VFQ: View all Fuel Queues.");
+        System.out.println("101 or VEQ: View all Empty Fuel Queues");
+        System.out.println("102 or ACQ: Add customers to the Queue.");
+        System.out.println("103 or RCQ: Remove a customer from a Queue.");
+        System.out.println("104 or PCQ: Remove a served customer.");
+        System.out.println("105 or VCS: View customers sorted alphabetically.");
+        System.out.println("106 or SPD: Store Program Data into file");
+        System.out.println("107 or LPD: Load Program Data from file.");
+        System.out.println("108 or STK: View Remaining Fuel Stock.");
+        System.out.println("109 or AFS: Add Fuel Stock.");
+        System.out.println("999 or EXT: Exit the Program.");
+
+        while (menuCount > -1) {
+
+            System.out.println("Enter your option  :");
+            String MenuOpt = scn.next();
+            MenuOpt=MenuOpt.toUpperCase();
+
+            if (MenuOpt.equals("100") || MenuOpt.equals("VFQ")) {
+                ViewFuelQueues(Pump, Pump1free, Pump2free, Pump3free);
+            } else if (MenuOpt.equals("101") || MenuOpt.equals("VEQ")) {
+                ViewEmptyQueue(Pump, Pump1free, Pump2free, Pump3free);
+            } else if (MenuOpt.equals("102") || MenuOpt.equals("ACQ")) {
+                AddCustomers(Pump);
+            } else if (MenuOpt.equals("103") || MenuOpt.equals("RCQ")) {
+                RemoveCustomers(Pump);
+
+            } else if (MenuOpt.equals("104") || MenuOpt.equals("PCQ")) {
+                RemoveServedCustomer(Pump);
+
+            } else if (MenuOpt.equals("105") || MenuOpt.equals("VCS")) {
+                SortedNames(Pump);
+
+            } else if (MenuOpt.equals("106") || MenuOpt.equals("SPD")) {
+                StoreDataIntoFile(Pump);
+
+            } else if (MenuOpt.equals("107") || MenuOpt.equals("LPD")) {
+                LoadData(Pump);
+
+            } else if (MenuOpt.equals("108") || MenuOpt.equals("STK")) {
+                RemainingFuel();
+
+            } else if (MenuOpt.equals("109") || MenuOpt.equals("AFS")) {
+                ADDFuel();
+
+            } else if (MenuOpt.equals("999") || MenuOpt.equals("EXT")) {
+                System.exit(0);
+
+            } else {
+                System.out.println("Invalid input!!!");
+            }
+
+            menuCount++;
+        }
+    }
 }
