ac76197a9ee6d5a83008ce7cf417402238c205a3
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-06 21:51:28 +0530

    arrays only

---
+++ Arrays Only/Task1/src/task1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class task1 {
+
+    static int x1 = 0;
+    static int FuelStock = 6600;
+    static int x2 = 7;
+    static int x3 = 14;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scn = new Scanner(System.in);
+
+        String[] Pump = new String[21];
+
+
+        int Pump1free = 0;
+        int Pump2free = 0;
+        int Pump3free = 0;
+}
