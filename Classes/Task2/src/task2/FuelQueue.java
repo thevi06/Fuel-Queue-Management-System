7fda0fe9f2a3382ffd83e55872f48aed0eb29737
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-08 22:05:07 +0530

    Update FuelQueue.java

--- Classes/Task2/src/task2/FuelQueue.java
+++ Classes/Task2/src/task2/FuelQueue.java
@@ -1,4 +1,31 @@
 package task2;
 
+import java.util.ArrayList;
+
 public class FuelQueue {
+    Passenger[] queue;
+    int id;
+
+    public FuelQueue(int id) {
+        this.queue = new Passenger[6];
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addPassenger(Passenger passenger) {
+        if(isFull()){
+            System.out.println("Queue is full");
+            return;
+        }
+
+        for (int i=0; i<this.queue.length; i++) {
+            if (queue[i] == null) {
+                queue[i] = passenger;
+                break;
+            }
+        }
+    }
 }
