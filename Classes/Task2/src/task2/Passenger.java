a91456ba8103d0df7d70ae5bec7c1c2aeb7f1961
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-08 22:02:46 +0530

    Update Passenger.java

--- Classes/Task2/src/task2/Passenger.java
+++ Classes/Task2/src/task2/Passenger.java
@@ -1,4 +1,35 @@
 package task2;
 
 public class Passenger {
+    private String firstName;
+    private String secondName;
+    private String vehicleNo;
+    private int requiredLiters;
+
+    public Passenger(String firstName, String secondName, String vehicleNo, int requiredLiters) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.vehicleNo = vehicleNo;
+        this.requiredLiters = requiredLiters;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+
+    public int getRequiredLiters() {
+        return requiredLiters;
+    }
+
+    public String getFullName() {
+        return String.format("%s %s", firstName, secondName);
+    }
 }
