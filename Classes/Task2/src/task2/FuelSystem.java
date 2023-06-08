c4a0d62c355a8c627d967ff98aaa2e30c8aa068d
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-08 22:04:07 +0530

    Update FuelSystem.java

--- Classes/Task2/src/task2/FuelSystem.java
+++ Classes/Task2/src/task2/FuelSystem.java
@@ -3,5 +3,31 @@ package task2;
 import java.util.ArrayList;
 
 public class FuelSystem {
-    
+    private int stock;
+    private FuelQueue[] pumps;
+    private ArrayList<String> customers;
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public FuelQueue[] getPumps() {
+        return pumps;
+    }
+
+    public void setPumps(FuelQueue[] pumps) {
+        this.pumps = pumps;
+    }
+
+    public ArrayList<String> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<String> customers) {
+        this.customers = customers;
+    }
 }
